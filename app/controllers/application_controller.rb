48079f02fdeefcb0dc22c428016bfe662fbab147
Raimundo Diaz <rai@F48C.local>
2020-06-23 20:47:18 -0400

    register update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
-  before_action :set_user
-  before_action :current_user
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
   def current_user
     @current_user = User.first
   end
-  def set_user
-    @user = User
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:full_name, :username, :age, :email, :password ])
   end
 end

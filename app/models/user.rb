aa46f4a18ef0b4c07534c68d0f63d2e1372460b7
Raimundo Diaz <rai@F48C.local>
2020-06-23 20:02:31 -0400

    sign_in forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,10 @@ end
 
 
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true, email: true
 

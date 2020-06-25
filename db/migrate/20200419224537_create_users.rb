194df0f19396ce650911f289dbdade008eba01f8
Raimundo Diaz <rai@F48C.local>
2020-06-25 02:04:26 -0400

    seed fixed

--- db/migrate/20200419224537_create_users.rb
+++ db/migrate/20200419224537_create_users.rb
@@ -4,7 +4,7 @@ class CreateUsers < ActiveRecord::Migration[6.0]
       t.string :username
       t.string :password
       t.string :full_name
-      t.string :email
+      #t.string :email
       t.integer :age
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_01_210519) do
+ActiveRecord::Schema.define(version: 2020_06_25_053951) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -117,10 +117,16 @@ ActiveRecord::Schema.define(version: 2020_06_01_210519) do
     t.string "username"
     t.string "password"
     t.string "full_name"
-    t.string "email"
     t.integer "age"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
